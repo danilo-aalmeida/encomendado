c692f390bf66e885f7f0b312de6011f3f0cc855e
Danilo A. Almeida <danilo.a.almeida@live.jp>
2019-10-10 20:55:17 -0300

    configuração inicial do router

--- front-end-application/src/App.vue
+++ front-end-application/src/App.vue
@@ -1,36 +1,14 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <v-app>
+      <h1>Você esta na página:</h1>
+      <router-view></router-view>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+  name: 'App'
+}
 </script>
---
+++ front-end-application/src/components/01-cadastro/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+        <v-layout>
+            <h3>Tela de Cadastro</h3>
+        </v-layout>     
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Cadastro',
+    data(){
+        
+    }     
+}
+</script>
